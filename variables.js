4ca8a4f2f3839f9e5bcba7f7b81c8cba6259ef0b
dev-ashik <61373940+dev-ashik@users.noreply.github.com>
2021-12-07 15:51:36 +0800

    first commit

---
+++ variables.js
@@ -0,0 +1,12 @@
+// Variables
+var name = "Md Ashik Mahmud"; // string
+console.log("Name: ", name);
+
+var age = 23; // int
+console.log("age", age);
+
+var weight = 75.4; // float
+console.log("weight: ", weight);
+
+var married = false; // boolean
+console.log("married: ", married);

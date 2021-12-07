32ddf331a89694321b79a6985baf045de4d55662
dev-ashik <61373940+dev-ashik@users.noreply.github.com>
2021-12-07 16:26:30 +0800

    variable

---
+++ dataType.js
@@ -0,0 +1,13 @@
+// primitive data type. 
+             //primitive types are deined by javaScript
+    // number
+    // string
+    // boolean (true, false)
+    // undefined
+    // null
+
+// object
+       // object types are user defined data type.
+    //    Array
+    //    Object
+    //    function
--- variables.js
+++ variables.js
@@ -1,4 +1,6 @@
 // Variables
+// JavaScript is a case Sensitive language
+
 var name = "Md Ashik Mahmud"; // string
 console.log("Name: ", name);
 
@@ -10,3 +12,20 @@ console.log("weight: ", weight);
 
 var married = false; // boolean
 console.log("married: ", married);
+
+// **** reserved word in js ****
+// asbtract, else,       instancedf, super
+// boolean,  enum,       int,        switch
+// break,    export,     interface,  synchronized
+// byte,     extends,    let,        this
+// case,     false,      long,       throw
+// catch,    final,      native,     throws
+// char,     finally,    new,        transient
+// class,    float,      null,       true
+// const,    for,        package,    try
+// continue, function,   private,    typeof
+// debugger, goto,       protected   var
+// default , if,         public,     void
+// delete,   implements, return,   volatile
+// do,       import,     short,    while
+// double,   in,         static,   with

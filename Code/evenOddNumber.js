a30bdf19544e26dec0785de414e178928f088399
dev-ashik <61373940+dev-ashik@users.noreply.github.com>
2021-12-10 10:11:22 +0800

    else if

---
+++ Code/evenOddNumber.js
@@ -0,0 +1,11 @@
+var number = 7;
+
+if (number === 0) {
+    console.log(number, "is Zero.");
+}
+else if (number%2 === 0) {
+    console.log(number, "is Even Number.");
+}
+else {
+    console.log(number, "is Odd Number.");
+}
---
+++ ifElse.js
@@ -0,0 +1,12 @@
+var number1 = 30;
+var number2 = 50;
+
+if (number1 > number2) {
+    console.log("number1 is gater than number2.");
+}
+else if (number2 > number1) {
+    console.log("number2 is gater than number1.");
+}
+else {
+    console.log("number1 and number2 are Equal.");
+}
